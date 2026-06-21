bd414b15e0f7fe87a9ff4122684a0b34ca8c8b01
PURUSHOTTAM <mahajanvaibhav619@gmail.com>
2026-06-21 22:51:37 +0530

    Binary
    Searching Method in O(log n) time complexity

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,6 +2,7 @@
 
 /*Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
+
 You must write an algorithm with O(log n) runtime complexity.
 
 
@@ -17,6 +18,7 @@ Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1*/
 
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 using namespace std;
